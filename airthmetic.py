02c84d772d88dab8c8716103243d93447d0a67b1
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-27 23:00:57 +0530

    Add files via upload

---
+++ airthmetic.py
@@ -0,0 +1,24 @@
+# Create a variable called a and set it equal to 20851 plus 92421
+a = 20851 + 92421
+
+# Create a variable called b and set it equal to 90234 minus 4314.5
+b = 90234 - 4314.5
+
+# Create a variable called c and set it equal to 5678 multiplied by 2341
+c = 5678 * 2341
+
+# Create a variable called d and set it equal to 993 divided by 456
+d = 993/456
+
+# Create a variable called e and set it equal to the remainder from 
+# dividing 500 by 3 
+e = 500%3
+
+#print a,b,c,d,equal
+
+print("a = ", a)
+print("b = ",b)
+print("c = ", c)
+print("d = ", d)
+print("e = ", e)
+
---
+++ exercise.py
@@ -0,0 +1,13 @@
+# First create a variable called x and set it equal to any number
+x = 5
+# Next create a variable called y and set it equal to any number
+y = 4
+
+# Now, create a variable called z and set it equal to x + y
+z = x + y
+
+# Now overwrite the value of x with the value of z
+x = z
+
+# Finally, print x to the screen using python's print function
+print(x)
