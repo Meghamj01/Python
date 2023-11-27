fd95dd305311dec60c5dc98187908dcb453729f0
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-27 22:04:46 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,12 @@
+
+
+first = 1 + 1
+
+print(first)
+
+
+second = 5 + 5
+print(second)
+
+total = first + second
+print(total)
---
+++ python_idle.py
@@ -0,0 +1,6 @@
+first = 1+1
+second = 2+5
+print("the sum of first numbers is : " +first)
+print("the sum of second number is: " +second)
+total = "first" + "second"
+total("the sum of all the number is : " +total)
