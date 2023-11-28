3ff6545ef167261b663670031d58be317531e4e5
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-28 11:41:02 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,21 @@
+import math
+print(math.floor(2.1))
+print(math.ceil(2.1))
+print(math.pi)
+print(math.e)
+print(math.sin(math.pi))
+print(math.sin(math.pi/2))
+
+      
+print(math.floor(math.sin(math.pi)))
+print(math.cos(0))
+print(math.asin(0))
+print(math.acos(0))
+print(math.hypot(3,4))
+print(math.pow(2,3))
+print(math.log(math.e))
+print(math.exp(2))
+print(math.log2(8))
+
+
+ 
