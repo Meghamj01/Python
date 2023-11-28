6153006e1224bacef794fb85ea419fdde1a8beee
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-28 12:57:43 +0530

    Add files via upload

---
+++ hello_you.py
@@ -0,0 +1,31 @@
+
+
+#TO ask your user for a name
+
+name = input("please enter your name = ")
+print(name)
+
+# to ask user for age
+
+age = input( "please enter your age name = ")
+print(age)
+
+# to ask user for a city
+city = input( "please enter your city name = ")
+print(city)
+
+# To ask user what they enjoy the most
+
+love = input(" what do you love the most during this course = ")
+print(love)
+
+#To ask user to create a textoutput text
+
+string = "your name is {} and you are {} years old. you live in {} and you love {}"
+output = string.format(name,age,city,love)
+
+
+#TO ask user to print output to the screen
+
+print(output)
+
