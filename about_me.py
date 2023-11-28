a5aaf597d28df42007897b20dc337f79b370efc4
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-28 13:18:56 +0530

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,26 @@
+# First, create a variable called start, and set it equal to "I am ".
+# Remember the space after the word "am" !
+start = "i am  "
+
+
+# Next, create a variable called age and set it equal to your age in years.
+# This must be a number
+age = 15
+
+
+# Next, create a variable called end, and set it equal to " years old".
+# Remember the space before the word "years"
+end = " years old."
+
+
+# Next, create a variable called output and use {} symbols and the format() function to stick the start, age and end variables
+
+
+# together to make a string.
+output = "{}{}{}".format(start,str(age),end)
+
+# An example output string would be "I am 15 years old"
+
+# Finally, print the output to the screen using the print() function.
+
+print(output)
