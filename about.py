9d1edb3c32345084ca6ae12138c75915c498c021
Megharaja MJ <123971550+Meghamj01@users.noreply.github.com>
2023-11-28 13:24:06 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,14 @@
+# Create a variable called name and set it equal to your name
+name = "Sarah"
+
+
+# Create a variable called age and set it equal to your age
+age = 22
+
+# Create a variable called output, and use {} symbols and the format() function to 
+
+# make it contain a string like the example text in the description
+output = "My name is {} and I am {} years old".format(name, age)
+
+# Finally, use the print() function to print the output.
+print(output)
